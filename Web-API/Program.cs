563b54c1fe7f3b3b5306ea5d0151f66183fd1d95
Plaksa Anastasia <plaksanasta13@gmail.com>
2024-01-16 16:08:53 +0300

    Add DbContext in Program.cs

--- Web-API/Program.cs
+++ Web-API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Web_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
