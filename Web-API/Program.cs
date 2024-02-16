7f80f660e04442174a6b88261650a75c86b5c000
Plaksa Anastasia <plaksanasta13@gmail.com>
2024-02-16 16:22:26 +0300

    End of 3 part

--- Web-API/Program.cs
+++ Web-API/Program.cs
@@ -11,13 +11,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();     
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));  
 
-app.UseAuthorization();
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
 
 app.MapControllers();
 
